11fa34c500d87f82c3b1ce3b4530e59949b7e71c
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-02-20 14:43:13 +0530

    feat: add langgraph implementation

--- 08_smartOps/index.js
+++ 08_smartOps/index.js
@@ -1,7 +1,13 @@
 import { totalSalesTool } from "./tools/salesTools.js"
 import { ChatGroq } from "@langchain/groq";
+import { StateGraph, StateSchema, MessagesValue } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 import dotenv from "dotenv";
 dotenv.config()
+
+const State = new StateSchema({
+    messages: MessagesValue
+})
 
 const llm = new ChatGroq({
   model: "openai/gpt-oss-120b",
@@ -10,40 +16,89 @@ const llm = new ChatGroq({
 
 const llmWithTools = llm.bindTools([totalSalesTool])
 
-const response = await llmWithTools.invoke([
-    {
-    role: "system",
-    content: "You are a business analytics assistant."
-  },
-  {
-    role: "user",
-    content: "What is the total sales?"
-  }
-])
+const llmCall = async(state)=>{
+    const response = await llmWithTools.invoke(state.messages)
 
-if(response?.tool_calls?.length){
-    const toolCall = response.tool_calls[0];
-    console.log("Tool requested:", toolCall.name);
-
-    const toolResult = await totalSalesTool.invoke(toolCall.args);
-
-    console.log("Tool Result:", toolResult);
-
-    const finalResponse = await llm.invoke([
-        {
-            role: "system",
-            content: "You are a business analytics assistant."
-        },
-        {
-            role: "user",
-            content: "What is the total sales?"
-        },
-        response,
-        {
-            role: "tool",
-            content: toolResult,
-            tool_call_id: toolCall.id
-        }
-    ])
-    console.log("Final Answer:", finalResponse.content);
+    return {
+        messages: [response]
+    }
 }
+
+const toolNode = new ToolNode([totalSalesTool])
+
+const shouldContinue = (state)=>{
+    const lastMessage = state.messages.at(-1);
+
+    if(lastMessage?.tool_calls?.length){
+        return "toolNode"
+    }
+    return "__end__"
+}
+
+// const response = await llmWithTools.invoke([
+//     {
+//     role: "system",
+//     content: "You are a business analytics assistant."
+//   },
+//   {
+//     role: "user",
+//     content: "What is the total sales?"
+//   }
+// ])
+
+// if(response?.tool_calls?.length){
+//     const toolCall = response.tool_calls[0];
+//     console.log("Tool requested:", toolCall.name);
+
+//     const toolResult = await totalSalesTool.invoke(toolCall.args);
+
+//     console.log("Tool Result:", toolResult);
+
+//     const finalResponse = await llm.invoke([
+//         {
+//             role: "system",
+//             content: "You are a business analytics assistant."
+//         },
+//         {
+//             role: "user",
+//             content: "What is the total sales?"
+//         },
+//         response,
+//         {
+//             role: "tool",
+//             content: toolResult,
+//             tool_call_id: toolCall.id
+//         }
+//     ])
+//     console.log("Final Answer:", finalResponse.content);
+// }
+
+const graph = new StateGraph(State)
+    .addNode("llmCall", llmCall)
+    .addNode("toolNode", toolNode)
+    .addEdge("__start__", "llmCall")
+    .addConditionalEdges(
+        "llmCall",
+        shouldContinue,
+        {
+           toolNode:  "toolNode",
+            __end__: "__end__"
+        }
+    )
+    .addEdge("toolNode", "llmCall")
+    .compile();
+
+const result = await graph.invoke({
+  messages: [
+    {
+      role: "system",
+      content: "You are a business analytics assistant."
+    },
+    {
+      role: "user",
+      content: "What is the total sales?"
+    }
+  ]
+});
+
+console.log(result.messages.at(-1).content);    
